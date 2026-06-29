b621325d1f75c0d44c77b4fddda2edbf31038b30
Yacouba <niampayacoub2018@gmail.com>
2026-06-29 06:07:18 +0000

    ok

---
+++ src/main/java/yamrestor/restor/RestorApplication.java
@@ -0,0 +1,13 @@
+package yamrestor.restor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestorApplication.class, args);
+	}
+
+}
